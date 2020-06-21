cc12b6365a88b8676827967b0a32ac2501d0f42e
yaakaito <yaakaito@gmail.com>
2020-06-21 23:48:16 +0900

    aa

---
+++ .github/actions/assign-reviewer/src/index.js
@@ -0,0 +1,34 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const action = require('./action')
+const { context } = require('@actions/github/lib/utils')
+
+const run = async () => {
+    try {
+        const { pull_request } = github.context.payload
+        if (!pull_request) {
+            core.setFailed('Not pull_request context.')
+            return
+        }
+
+        const options = {
+            context: {
+                repo: process.env.GITHUB_REPOSITORY,
+                pull_number: pull_request.number
+            },
+            teamReviewers: core.getInput('teamReviewers') || undefined,
+            reviewers: core.getInput('reviewers') || undefined,
+            token: core.getInput('githubToken')
+        }
+        console.log(options)
+
+        const reviewers = await action(options)
+        core.setOutput('reviewers', (reviewers || []).map(r => `@${r}`).join(' '))
+    }
+    catch(e) {
+        core.setFailed(e.message)
+    }
+
+}
+
+run()
